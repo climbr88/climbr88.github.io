aac920b7520189254b239c3958d71d6eec46a4c9
unknown <winyim@icloud.com>
2023-09-07 15:14:33 -0400

    updated app2.py

--- app2.py
+++ app2.py
@@ -70,9 +70,9 @@ def index():
     if form.validate_on_submit():
         customer_name = form.customer.data
         files_html, teu_html = update_chart(customer_name)
-        return render_template('index2.html', form=form, files_html=files_html, teu_html=teu_html)
+        return render_template('index.html', form=form, files_html=files_html, teu_html=teu_html)
     else:
-        return render_template('index2.html', form=form)
+        return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
