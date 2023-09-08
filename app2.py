e16210506fb8835559504f0cd21093f8f945a449
climbr88 <50963894+climbr88@users.noreply.github.com>
2023-09-08 17:27:37 -0400

    Update app2.py
    updated excel file location

--- app2.py
+++ app2.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = '202eagle'  # Set your secret key
 
 # Read the Excel sheet into a pandas dataframe
-df = pd.read_excel('data/filename.xlsx', header=1)
+df = pd.read_excel('docs/filename.xlsx', header=1)
 
 # Convert the date columns to datetime format
 df['ETA'] = pd.to_datetime(df['ETA'])
