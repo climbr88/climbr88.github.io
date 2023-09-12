a81bc7d9c32d4643470dfba5ad800497891eb363
unknown <winyim@icloud.com>
2023-09-12 16:29:26 -0400

    move index.html from folder

--- app2.py
+++ app2.py
@@ -4,9 +4,11 @@ from wtforms import SelectField
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
+import os
 
 app = Flask(__name__)
 app.secret_key = '202eagle'  # Set your secret key
+  
 
 # Read the Excel sheet into a pandas dataframe
 df = pd.read_excel('data/filename.xlsx', header=1)
@@ -75,4 +77,7 @@ def index():
         return render_template('docs/index.html', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
+    #app.run(debug=True)
